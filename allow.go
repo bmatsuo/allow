b700a5c7682de5e0f547930ac29711fcd5500e86
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-05-20 00:22:15 -0700

    godoc

--- allow.go
+++ allow.go
@@ -3,13 +3,11 @@ Package allow provides HTTP method routing.
 
 This API is experimental.
 
-There are really two distinct APIs here. The Map* functions use map literal
+There are two APIs in the allow package. The Map* functions use map literal
 syntax for clean, structured routes.  The Allow type has a fluent interface and
 allows for http.Handler and http.HandlerFunc types to be used more freely.
 
-Functions in the package issue runtime panics.  It is not planned for this API
-to support runtime modification of top-level routing logic.  An early runtime
-panic is the best action to take if routes are static and built on startup.
+Functions in the allow package issue runtime panics.
 */
 package allow
 
